184df2c733b1718502adbe9c91c0d7602d01780b
thisischina <874114275@qq.com>
2017-06-13 16:10:02 +0800

    2017年6月13日16:08:20

--- src/main/java/com/woodmusic/service/impl/UserServiceImpl.java
+++ src/main/java/com/woodmusic/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.woodmusic.service.impl;
 
 import com.woodmusic.dao.UserDao;
 import com.woodmusic.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Administrator on 2017/6/5 0005.
  */
 public class UserServiceImpl implements UserService{
+    @Autowired
     public UserDao userDao;
     public int addUser() {
         return userDao.add();
--- src/main/java/com/woodmusic/web/UserController.java
+++ src/main/java/com/woodmusic/web/UserController.java
@@ -1,17 +1,24 @@
 package com.woodmusic.web;
 
 import com.woodmusic.service.UserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2017/6/5 0005.
  */
-@RequestMapping("user")
+@Controller
+@RequestMapping("pages/html")
 public class UserController {
     private UserService userService;
 
+    @ResponseBody
+    @RequestMapping("/add")
     public String add(){
+        System.out.println("开始注册+");
         int i=userService.addUser();
+        System.out.println("添加成功"+i);
         return i>0?"success":"error";
     }
 }
